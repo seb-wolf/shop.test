dc072cc873348ac037202182595613c12af1af9b
seb-wolf <sebsggw@gmail.com>
2024-01-16 19:29:15 +0100

    Added pagination functionality

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ const mix = require('laravel-mix')
 
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/delete.js', 'public/js')
+    .js('resources/js/welcome.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css');
 
 
